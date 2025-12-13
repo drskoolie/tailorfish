66997bd5a1142757e83db4760986a6ee553a3d61
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-12-13 16:30:56 -0500

    All tests passing

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,43 +2,46 @@ from pathlib import Path
 
 import chess
 
-from tailorfish.analysis import detect_blunders, detect_missed_moves
-from tailorfish.eval import StockfishEvaluator
+from tailorfish.analysis import GameEvaluator
 
 
 def test_detect_blunder_true() -> None:
     pgn_path = Path("tests/fixtures/blunder.pgn")
-    with open(pgn_path) as pgn:
-        game = chess.pgn.read_game(pgn)
 
-    blunders = detect_blunders(game, StockfishEvaluator, chess.WHITE)
+    ge = GameEvaluator()
+    ge.set_game(pgn_path, chess.WHITE)
+
+    blunders = ge.detect_blunders()
 
     assert len(blunders) == 1
 
 def test_detect_blunder_false() -> None:
     pgn_path = Path("tests/fixtures/no_blunder.pgn")
-    with open(pgn_path) as pgn:
-        game = chess.pgn.read_game(pgn)
 
-    blunders = detect_blunders(game, StockfishEvaluator, chess.WHITE)
+    ge = GameEvaluator()
+    ge.set_game(pgn_path, chess.WHITE)
+
+    blunders = ge.detect_blunders()
 
     assert len(blunders) == 0
 
 def test_detect_missed_move_true() -> None:
     pgn_path = Path("tests/fixtures/missed_move.pgn")
-    with open(pgn_path) as pgn:
-        game = chess.pgn.read_game(pgn)
 
-    missed_moves = detect_missed_moves(game, StockfishEvaluator, chess.WHITE)
+    ge = GameEvaluator()
+    ge.set_game(pgn_path, chess.WHITE)
+
+    missed_moves = ge.detect_missed_moves()
 
     assert len(missed_moves) == 1
 
 
 def test_detect_missed_move_false() -> None:
     pgn_path = Path("tests/fixtures/no_blunder.pgn")
-    with open(pgn_path) as pgn:
-        game = chess.pgn.read_game(pgn)
 
-    missed_moves = detect_missed_moves(game, StockfishEvaluator, chess.WHITE)
+    ge = GameEvaluator()
+    ge.set_game(pgn_path, chess.WHITE)
+
+    missed_moves = ge.detect_missed_moves()
 
     assert len(missed_moves) == 0
