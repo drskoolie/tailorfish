0b685dfa2c1a5a08f5cbcad55c3bfbeed1b4b38f
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-12-13 22:30:47 -0500

    Add some display of board

--- src/tailorfish/app.py
+++ src/tailorfish/app.py
@@ -1,18 +1,61 @@
-from pathlib import Path
+import chess
+from textual.app import App, ComposeResult
+from textual.binding import Binding
+from textual.widgets import Static
 
-from textual.app import App
+WHITE_COLOR = "yellow"
+BLACK_COLOR = "bright_black"  # or "brown", "red", "orange1"
 
-from tailorfish.analysis import GameEvaluator, load_games_into_ram
+def board_to_ascii(board: chess.Board) -> str:
+    lines: list[str] = []
+
+    for rank in range(7, -1, -1):
+        row: list[str] = []
+        for file in range(8):
+            sq = chess.square(file, rank)
+            piece = board.piece_at(sq)
+
+            if piece is None:
+                row.append(".")
+            else:
+                sym = piece.symbol()
+                if sym.isupper():
+                    row.append(f"[{WHITE_COLOR}]{sym}[/{WHITE_COLOR}]")
+                else:
+                    row.append(f"[{BLACK_COLOR}]{sym}[/{BLACK_COLOR}]")
+
+        lines.append(f"{rank+1}  " + " ".join(row))
+
+    lines.append("")
+    lines.append("   a b c d e f g h")
+    return "\n".join(lines)
+
+
 
 class TailorfishTUI(App):
     BINDINGS = [
-            ("q", "quit", "Quit")
+            ("q", "quit", "Quit"),
             ]
 
-    def action_quit(self):
+    def __init__(self) -> None:
+        super().__init__()
+        self.board = chess.Board()
+
+    def compose(self) -> ComposeResult:
+        self.board_view = Static()
+        yield self.board_view
+
+    def on_mount(self) -> None:
+        self.board_view.update(board_to_ascii(self.board))
+
+    def action_quit(self) -> None:
         self.exit()
 
 if __name__ == "__main__":
+    from pathlib import Path
+
+    from tailorfish.analysis import GameEvaluator, load_games_into_ram
+
     if False:
         pgn_path_drskoolie = Path("data/raw/lichess_drskoolie_2025-12-14.pgn")
         games = load_games_into_ram(pgn_path_drskoolie)
