a482ef0bbace31888272ab95d07b4ec96fcaaa7e
Al-Baraa El-Hag <a.elhag01@gmail.com>
2025-12-28 18:26:22 -0500

    Add class

--- src/tailorfish/tui.py
+++ src/tailorfish/tui.py
@@ -2,41 +2,59 @@ import chess
 from rich import box
 from rich.console import Console
 from rich.table import Table
+from rich.text import Text
 
-def rank_row(board: chess.Board, rank: int):
-    row = ""
-    for file_idx in range(8):  # a → h
-        square = chess.square(file_idx, rank)
-        piece = board.piece_at(square)
-        row += piece.symbol() if piece else " "
-    return row
+class ChessTable():
+    def __init__(self, board: chess.Board):
+        self.board = board
+
+        self.files = " ABCDEFGH "
+
+    def make_table(self) -> Table:
+        table = Table(
+                title="Chess", 
+                box=box.ROUNDED,
+                show_lines=True,
+                )
+
+        for file in self.files:
+            table.add_column(file, justify="center", no_wrap=True)
+
+
+        for rank_idx in range(7, -1, -1):
+            label = str(rank_idx + 1)
+            files = self.get_square_rank(rank_idx)
+            table.add_row(label, *files, label)
+
+        table.add_row(*self.files)
+
+        return table
+
+
+    def get_square_cell(self, file_idx: int, rank_idx: int) -> Text:
+        sq = chess.square(file_idx, rank_idx)
+        p = self.board.piece_at(sq)
+
+        p = p if p else " "
+
+        light = (file_idx + rank_idx) % 2 == 0
+        base_style = "on magenta black" if light else "on yellow black"
+
+        return Text(f" {p} ", style=base_style)
+
+    def get_square_rank(self, rank_idx: int) -> list[Text]:
+        files = []
+
+        for file in range(0,8):
+            files.append(self.get_square_cell(file, rank_idx))
+
+        return files
 
 
 board = chess.Board()
-rank_row(board, 0)
-
-
 
 console = Console()
-table = Table(
-        title="Chess", 
-        box=box.ROUNDED,
-        show_lines=True,
-        )
 
+chess_table = ChessTable(board)
 
-ranks = "12345678"
-files = " ABCDEFGH "
-
-
-for file in files:
-    table.add_column(file)
-
-
-for rank_no, rank in enumerate(reversed(ranks)):
-    table.add_row(rank, *rank_row(board, rank_no), rank)
-
-table.add_row(*files)
-
-console.print(table)
-
+console.print(chess_table.make_table())
